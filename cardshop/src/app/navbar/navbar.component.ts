12b30bede097080cd6ea018f71b3f7a2dcfd4ce7
CroiXan <31298324+CroiXan@users.noreply.github.com>
2024-05-31 00:46:22 -0400

    accion de busqueda por nombre

--- cardshop/src/app/navbar/navbar.component.ts
+++ cardshop/src/app/navbar/navbar.component.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
+import { FormGroup, ReactiveFormsModule, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  filterForm!: FormGroup;
+
+  constructor(private router: Router){};
+
+  ngOnInit(): void {
+    this.filterForm = new FormGroup({
+      search: new FormControl('')
+    });
+  }
+
+  onSearch(){
+    const search = this.filterForm.get('search')?.value.toLowerCase() || '';
+    if(search !== ''){
+      this.router.navigate(['/buscar/'+search]);
+    }
+  }
 
 }
