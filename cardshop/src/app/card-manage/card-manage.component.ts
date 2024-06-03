2b980f1b691dd571f4a59dee96b7fb8311bfc3bb
CroiXan <31298324+CroiXan@users.noreply.github.com>
2024-06-03 00:24:03 -0400

    listado de cartas en el administrador

--- cardshop/src/app/card-manage/card-manage.component.ts
+++ cardshop/src/app/card-manage/card-manage.component.ts
@@ -1,28 +1,46 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../core/services/auth.service';
+import { CardItem } from '../core/models/carditem.model';
+import { CardItemService } from '../core/services/cartitem.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-manage',
   standalone: true,
   imports: [
+    CommonModule
   ],
   templateUrl: './card-manage.component.html',
   styleUrl: './card-manage.component.css'
 })
 export class CardManageComponent {
 
+  cardList: CardItem[] = [];
+
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private cardItemService: CardItemService
   ){}
 
+  ngOnInit(): void {
+    this.cardList = this.cardItemService.getCardsList();
+  }
+
+  setSelecetedCard(card: CardItem){
+    this.authService.setSelectedCardForManage(card);
+    this.goToDetail();
+  }
+
   goToDetail(){
-    if(this.authService.getUser().id === undefined){
+    if(this.authService.getUser().id === undefined || 
+      this.authService.getUser().Role != 'admin'){
       alert('No puede ingresar a esta funcion');
       this.router.navigate(['/login']);
+    }else{
+      this.router.navigate(['/card-detail']);
     }
-    this.router.navigate(['/card-detail']);
   }
   
 }
