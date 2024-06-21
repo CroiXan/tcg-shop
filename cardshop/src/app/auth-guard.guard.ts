34f7245d59bb63630d4b4bd2dc157f7992fdb943
CroiXan <31298324+CroiXan@users.noreply.github.com>
2024-06-20 22:48:22 -0400

    documentacion de la capa models

--- cardshop/src/app/auth-guard.guard.ts
+++ cardshop/src/app/auth-guard.guard.ts
@@ -3,6 +3,12 @@ import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './core/services/auth.service';
 
+/**
+ * Uso de CanActivateFn para validar si se el usuario ingreso al sitio.
+ * @param route 
+ * @param state 
+ * @returns boolean
+ */
 export const authGuardGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
--- cardshop/src/app/core/models/address.model.ts
+++ cardshop/src/app/core/models/address.model.ts
@@ -1,8 +1,30 @@
+/**
+ * @description
+ * Objeto para definir la estructura para la informacion de entrega de los usuarios.
+ */
 export interface Address{
+    /**
+     * Identificador de objeto.
+     */
     id: number;
+    /**
+     * Valor numerico que se corresponde al id del Objeto User.
+     */
     UserId: number;
+    /**
+     * Nombre de direccion.
+     */
     Name: string;
+    /**
+     * Numero de direccion.
+     */
     Number: number;
+    /**
+     * Region de direccion.
+     */
     Region: string;
+    /**
+     * Comuna de direccion.
+     */
     Commune: string;
 }
