5dc6e37fdf1240fa51183c133af18c85d63d4404
CroiXan <31298324+CroiXan@users.noreply.github.com>
2024-07-04 23:53:01 -0400

    funciones de api de direcciones

--- cardshop/src/app/core/services/api/address-api.service.ts
+++ cardshop/src/app/core/services/api/address-api.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Address } from '../../models/address.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,4 +18,58 @@ export class AddressApiService {
   private url = 'https://firebasestorage.googleapis.com/v0/b/mtg-shop-d23a4.appspot.com/o/address.json?alt=media&token=177adff0-9ba2-4243-b2f9-00ce0410a300';
 
   constructor(private http: HttpClient) { }
+
+  getAddress(): Observable<Address[]>{
+    return this.http.get<Address[]>(this.url);
+  }
+
+  editAddressJson(addressList: Address[]): Observable<any>{
+    return this.http.post(this.url,addressList,this.httpOptions);
+  }
+
+  createAddress(
+    userId: number, 
+    name: string, 
+    number: number, 
+    region: string, 
+    Commune: string){
+
+      let newAddress = {} as Address;
+
+      this.getAddress().subscribe(
+        response => {
+          const newId = response.reduce((maxId, user) => {
+            return Math.max(maxId, user.id);
+          }, 0) + 1;
+    
+          newAddress.id=newId;
+          newAddress.UserId = userId;
+          newAddress.Name = name;
+          newAddress.Number = number;
+          newAddress.Region = region;
+          newAddress.Commune = Commune;
+          
+          
+          response.push(newAddress);
+
+          this.editAddressJson(response);
+        },
+        error => {
+
+        }
+      );
+      
+  }
+
+  getAddressByUser(userId: number, callback: (result: Address[]) => void) {
+    this.getAddress().subscribe(
+      result => {
+        callback(result.filter(address => address.UserId === userId) || [] as Address[]);
+      },
+      error => {
+        callback([] as Address[]);
+      }
+    );
+  }
+
 }
