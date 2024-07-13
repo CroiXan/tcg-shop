baa83c65e96dd28319fd13c81d75b38c066e30d8
CroiXan <31298324+CroiXan@users.noreply.github.com>
2024-07-13 12:31:42 -0400

    fix test httpclient

--- cardshop/src/app/core/services/api/cards.service.spec.ts
+++ cardshop/src/app/core/services/api/cards.service.spec.ts
@@ -1,9 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 import * as CardItemJsonList from '../../../assets/cards.json';
 import { CardsService } from './cards.service';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CardItem } from '../../models/carditem.model';
-import { BehaviorSubject, Subject, of } from 'rxjs';
+import { of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 describe('CardsService', () => {
@@ -11,8 +11,19 @@ describe('CardsService', () => {
   let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get','post']);
-    service = new CardsService(httpClientSpy);
+    let httpClientSpyObj = jasmine.createSpyObj('HttpClient', ['get','post']);
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [ CardsService ,
+        {
+          provide: HttpClient,
+          useValue: httpClientSpyObj
+        }
+      ]
+    });
+
+    service = TestBed.inject(CardsService);
+    httpClientSpy = TestBed.inject(HttpClient) as jasmine.SpyObj<HttpClient>;
   });
 
   it('should be created', () => {
@@ -31,30 +42,31 @@ describe('CardsService', () => {
 
   });
 
-  it('Editar Carta en Catalogo', () => {
+  it('Editar Carta en Catalogo', (done) => {
     let dummyData = CardItemJsonList as CardItem[];
 
     httpClientSpy.get.and.returnValue(of(dummyData));
 
     dummyData[0].CardName = 'OKO';
 
-    httpClientSpy.post.and.returnValue(new Subject<any>().asObservable());
+    httpClientSpy.post.and.returnValue(of(dummyData));
 
     service.editCardsJson(dummyData).subscribe(data => {
       service.getAllCards().subscribe(data => {
         expect(data.length).toBe(21);
         expect(data[0].CardName).toEqual('OKO');
+        done();
       });
     });
 
   });
 
-  it('Crear Carta en Catalogo', () => {
+  it('Crear Carta en Catalogo', (done) => {
     let dummyData = CardItemJsonList as CardItem[];
     let newCard = {} as CardItem;
 
     httpClientSpy.get.and.returnValue(of(dummyData));
-    httpClientSpy.post.and.returnValue(new Subject<any>().asObservable());
+    httpClientSpy.post.and.returnValue(of(dummyData));
 
     newCard.CardName = 'Prueba';
     newCard.CardNumber = 123;
@@ -65,11 +77,11 @@ describe('CardsService', () => {
 
     service.editCardsJson(dummyData).subscribe(data => {
       service.getAllCards().subscribe(data => {
-        expect(data.length).toBe(22);
         expect(data[21].CardName).toEqual('Prueba');
         expect(data[21].CardNumber).toBe(123);
         expect(data[21].SetName).toEqual('Testing');
         expect(data[21].SetCode).toEqual('TST');
+        done();
       });
     });
 
